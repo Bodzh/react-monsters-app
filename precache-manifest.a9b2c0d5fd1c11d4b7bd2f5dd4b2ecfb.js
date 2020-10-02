d13ae482fadd327b4cc7ac19b4a1b84f99732d11
Bodzh <bodzhe@gmail.com>
2020-10-02 11:24:49 +0300

    Updates

---
+++ precache-manifest.a9b2c0d5fd1c11d4b7bd2f5dd4b2ecfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6e553bcbd3c78fed87ce3ddfc8b7f2",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "7a91960d0c45fa2c9ef8",
+    "url": "/react-monsters-app/static/css/main.0f495794.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/react-monsters-app/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-app/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a91960d0c45fa2c9ef8",
+    "url": "/react-monsters-app/static/js/main.fdc7bb53.chunk.js"
+  },
+  {
+    "revision": "7aeec754363787488e6a",
+    "url": "/react-monsters-app/static/js/runtime-main.824356a6.js"
+  }
+]);
